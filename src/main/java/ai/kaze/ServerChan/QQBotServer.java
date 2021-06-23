dffb2b972c3886d80a721d3b82f3d96a160c2760
Li Keqing <me@kaze.ai>
2021-06-23 15:39:44 +0800

    initial commit

---
+++ src/main/java/ai/kaze/ServerChan/QQBotServer.java
@@ -0,0 +1,40 @@
+package ai.kaze.ServerChan;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+public class QQBotServer extends WebSocketServer {
+    ServerChanPlugin plugin;
+
+    QQBotServer(int port) {
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+
+    }
+
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+
+    }
+
+    @Override
+    public void onStart() {
+        plugin.logger.info("");
+    }
+}
---
+++ src/main/java/ai/kaze/ServerChan/ServerChanPlugin.java
@@ -0,0 +1,52 @@
+package ai.kaze.ServerChan;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.logging.Logger;
+
+public class ServerChanPlugin extends JavaPlugin {
+    final Logger logger = Logger.getLogger("Minecraft");
+    QQBotServer server;
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        if (server != null) {
+            try {
+                server.stop();
+            } catch (IOException | InterruptedException e) {
+                logger.severe(e.getMessage());
+                return;
+            }
+        }
+        server = new QQBotServer(getConfig().getInt("port", 10000));
+        server.plugin = this;
+        server.start();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        if (server != null) {
+            try {
+                server.stop();
+                server = null;
+            } catch (IOException | InterruptedException e) {
+                logger.severe(e.getMessage());
+            }
+        }
+    }
+
+    public void dispatchCommand(String command) {
+
+    }
+}
